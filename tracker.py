5fe461b664a5444aafe023329b1b836d276b6309
Niaev <trutnv@protonmail.com>
2020-07-31 00:44:21 -0300

    tracker.py first steps

--- tracker.py
+++ tracker.py
@@ -1,1 +1,54 @@
 # imports
+## link_tracking modules
+from crawler import Crawler, scrape_list
+## others
+import sys, os
+
+# parameters handling
+if len(sys.argv) == 1: 
+    print('No parameters were given')
+    sys.exit()
+elif len(sys.argv) == 2 and sys.argv[1] != 'help!':
+    depht = 5
+    seeds_file = sys.argv[1]
+elif len(sys.argv) == 3:
+    depht = sys.argv[2]
+    seeds_file = sys.argv[1]
+elif len(sys.argv) == 2 and sys.argv[1] == 'help!':
+    print('''
+
+# about
+
+link_tracking is a web crawling tool that requires a list of seed links to start crawling and find new links and contents, that are cured and indexed
+
+# how to use
+
+it's simple to use. just execute it and then you can give parameters
+
+    $ python3 tracker.py SEEDS_FILE [DEPHT]
+
+SEEDS_FILE is the path of a text file with a few links in it, one link by line
+DEPHT is a optional parameter. it is an integer number that defined de depht of the crawling inside each seed link. the default DEPHT is 5
+
+or you can type help! as a parameter to get this message in your terminal
+
+            ''')
+    sys.exit()
+
+# reading seeds file
+try:
+    with open(seeds_file, 'r') as f:
+        seeds = f.read().split('\n')[0:-1]
+except FileNotFoundError:
+    print('seeds file was not found')
+    sys.exit()
+
+# links list
+l = []
+
+# crawling seeds
+for seed in seeds:
+    c = Crawler(seed)
+    c.track()
+    l = l + c.links
+print(l)
