efc1d26d465f9ace44aef3fc00ef3c57b9d8752a
Niaev <trutnv@protonmail.com>
2020-10-11 19:18:05 -0300

    indexer.py class creation

--- indexer.py
+++ indexer.py
@@ -1,1 +1,18 @@
-# imports
+# general imports
+import re
+
+class Indexer():
+    '''
+        Class that receives a list of links to organize and index
+
+        ### Attributes
+
+        * links
+            * `list`
+            * list of links
+        
+    '''
+
+    def __init__(self, links):
+
+        self.links = links
