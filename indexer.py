bbe83a372765c7f6e6bca770faeba9e02896e3b3
Niaev <trutnv@protonmail.com>
2020-10-11 19:41:31 -0300

    indexer.py removing duplicates and invalid links at class init

--- indexer.py
+++ indexer.py
@@ -1,6 +1,10 @@
 # general imports
 import re
 
+# web crawling imports
+from urllib.request import urlopen
+from urllib.error import HTTPError, URLError
+
 class Indexer():
     '''
         Class that receives a list of links to organize and index
@@ -16,3 +20,23 @@ class Indexer():
     def __init__(self, links):
 
         self.links = links
+        self.remove_duplis()
+        self.valid_links()
+
+    def remove_duplis(self):
+        '''
+            Remove duplicates in `self.links`
+        '''
+        self.links = list(set(self.links))
+    
+    def valid_links(self):
+        '''
+            Maintains only valid links in `self.links` by removing invalid ones
+        '''
+
+        for i,link in enumerate(self.links):
+            print(i)
+            try:
+                html = urlopen(link)
+            except (ValueError, HTTPError, URLError) as e:
+                self.links.pop(i)
